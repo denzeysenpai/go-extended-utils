c088ff0fd6e66623411c7d9cd1f574f1af954f37
DenzeySenpai <marksolante221@gmail.com>
2025-04-23 11:53:17 +0800

    [0.2.3 : Date Time]

--- date-time/date_time.go
+++ date-time/date_time.go
@@ -1,12 +1,36 @@
 package datetime
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func Get_yyyy_mm() string {
 	return time.Now().Format("2006_01")
 }
 
-func Sleep(pause int, start time.Time) {
+func SleepAt(pause int, start time.Time) {
 	sleep := (time.Duration(pause) * time.Second) - time.Now().Sub(start)
 	time.Sleep(sleep)
+}
+
+func SleepNow(pause int) {
+	sleep := (time.Duration(pause) * time.Second) - time.Now().Sub(time.Now())
+	time.Sleep(sleep)
+}
+
+func DateCode() string {
+	var strTemp string
+	defer func() {
+		if r := recover(); r != nil {
+			strTemp = ""
+		}
+	}()
+	now := time.Now()
+	calcDateTime := now.Year() - 2016
+	calcDateTime = (calcDateTime * 12) + int(now.Month())
+	calcDateTime = (calcDateTime * 32) + now.Day()
+	strTemp = fmt.Sprintf("%04d", calcDateTime)
+
+	return strTemp
 }
