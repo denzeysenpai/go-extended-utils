ed7f6bdde399f1ffb923b8ac5a4acf30144544ff
DenzeySenpai <marksolante221@gmail.com>
2025-04-23 11:53:56 +0800

    [0.2.5 : Generation]

--- generate/generation.go
+++ generate/generation.go
@@ -4,19 +4,16 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-func Generate_number_string(limit int) string {
-	rand.Seed(time.Now().UnixNano())
+func GenerateNumberString(limit int) string {
 	result := "6"
 	for i := 0; i < limit; i++ {
 		result += fmt.Sprintf("%d", rand.Intn(10))
 	}
-
 	return result
 }
-func Generate_SHA1(fields ...string) string {
+func GenerateSHA1(fields ...string) string {
 	concatenated := ""
 	for _, field := range fields {
 		concatenated += field
